0f5e5357bca0b96a91901ddb4fd0313161f970e4
arvind <apulijala>
2021-11-01 18:49:15 +0000

    finished work

--- kubemaster-init.sh
+++
@@ -1,8 +0,0 @@
-lsmod | grep br_netfilter
-sudo modprobe br_netfilter
-lsmod | grep br_netfilter
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sudo sysctl --system
